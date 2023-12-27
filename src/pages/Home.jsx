744c89d9e82e6f838a829259eda7548bddd1d915
ritharam <119063944+ritharam@users.noreply.github.com>
2023-12-27 17:12:19 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div>
     <Navbar/>
       <div className="flex flex-col md:flex-row bg-[#f5f5f5] pt-24">
-        <div className="max-w-1/2 p-20">
+        <div className="max-w-1/2 md:p-20 p-10">
           <div className="font-[IntroScript] text-6xl font-bold text-[#05acb4]">Unitastic</div>
           <div className="font-[FontSpring] text-4xl font-bold py-2">A one stop solution for all your university needs</div>
           <div className="border-t-[6px] border-[#05acb4] w-1/4 py-2"></div>
@@ -43,7 +43,7 @@ function Home() {
 
       <div className="flex flex-col md:flex-row bg-[#f5f5f5]">
         <img src="/assets/images/library.svg" alt="CS" className="md:max-w-1/2 lg:w-1/2 md:items-center" />
-        <div className="max-w-1/2 p-20">
+        <div className="max-w-1/2 md:p-20 p-10">
           <div className="font-[IntroScript] text-5xl font-bold text-[#05acb4]">Materials</div>
           <div className="font-normal text-[#6a7070] text-[18px] font-['FontSpring'] py-5">
             <p>Get all the materials you need for your university life, be it notes, previous question papers,
@@ -72,7 +72,7 @@ function Home() {
         </div>
 
         <div className="flex flex-col md:flex-row bg-[#f5f5f5]">
-        <div className="max-w-1/2 p-20">
+        <div className="max-w-1/2 md:p-20 p-10">
           <div className="font-[IntroScript] text-5xl font-bold text-[#05acb4]">Utilities</div>
           <div className="font-normal text-[#6a7070] text-[18px] font-['FontSpring'] py-5">
             <p>Calculate your SGPA, CGPA, find out how many classes you can skip, and much more with Unitastic's utilities.</p>
